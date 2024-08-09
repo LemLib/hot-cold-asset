b9c9662c89a7311449ee443729f6a8dc2f18eaf7
Liam Teale <111480281+SizzinSeal@users.noreply.github.com>
2024-08-09 13:52:17 -0700

    remove gif-pros

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,9 @@
 #include "main.h"
-
 #include "asset.h"
-#include "gif-pros/gifclass.hpp"
-#include "liblvgl/lvgl.h"
 
 ASSET_LIB(chip_gif)
 ASSET(hot_chip_gif)
 
 void initialize() {}
 
-void opcontrol() {
-  Gif gif(chip_gif, lv_scr_act());
-
-  while (true) {
-    pros::delay(20);
-  }
-}
+void opcontrol() {}
